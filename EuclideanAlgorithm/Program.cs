149e57b5ee609e5a6193b321ee03ec37fd30b46c
TenDan <tytusworld@gmail.com>
2021-08-29 12:10:05 +0200

    Add EuclideanAlgorithm

---
+++ EuclideanAlgorithm/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Euclidean_algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new Stopwatch();
+            int a, b;
+            do
+            {
+                Console.Write("Specify two numbers: ");
+
+                var line = Console.ReadLine().Split(" ")[0..2];
+
+                if (
+                    int.TryParse(line[0], out a)
+                    && int.TryParse(line[1], out b)
+                   )
+                    break;
+
+                Console.WriteLine("Both values must be numbers");
+
+            } while (true);
+
+            // Modulo method
+            sw.Start();
+
+            Console.WriteLine("Greatest Common Divisor of {0} and {1} is {2} (Modulo method)", a, b, GreatestCommonDivisorWithModulo(a,b));
+
+            sw.Stop();
+            Console.WriteLine("Elapsed: {0}", sw.Elapsed);
+
+            // Comparison method
+            sw.Restart();
+
+            Console.WriteLine("Greatest Common Divisor of {0} and {1} is {2} (Comparison method)", a, b, GreatestCommonDivisorWithValueComparison(a,b));
+
+            sw.Stop();
+            Console.WriteLine("Elapsed: {0}", sw.Elapsed);
+
+            // Recurention method
+            sw.Restart();
+
+            Console.WriteLine("Greatest Common Divisor of {0} and {1} is {2} (Recurention method)", a, b, GreatestCommonDivisorRecursive(a,b));
+
+            sw.Stop();
+            Console.WriteLine("Elapsed: {0}", sw.Elapsed);
+        }
+        static int GreatestCommonDivisorWithModulo(int number1, int number2)
+        {
+            int temp;
+            while (number2 != 0)
+            {
+                temp = number2;
+                number2 = number1 % number2;
+                number1 = temp;
+            }
+            return number1;
+        }
+        static int GreatestCommonDivisorWithValueComparison(int number1, int number2)
+        {
+            while (number1 != number2)
+            {
+                if (number1 > number2)
+                    number1 = number1 - number2;
+                else
+                    number2 = number2 - number1;
+            }
+            return number1;
+        }
+        static int GreatestCommonDivisorRecursive(int number1, int number2)
+        {
+            if (number2 == 0)
+                return number1;
+            else
+                return GreatestCommonDivisorRecursive(number2, number1 % number2);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 Currently implemented:
 - Sieve or Eratosthenes [Wikipedia](https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes)
+- Euclidean algorithm [Wikipedia](https://en.wikipedia.org/wiki/Euclidean_algorithm)
 
 Work in progress:
-- Euclidean algorithm [Wikipedia](https://en.wikipedia.org/wiki/Euclidean_algorithm)
+None
